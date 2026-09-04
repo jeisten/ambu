bb0f37c9f8e6ccd7ef908eabb5d2cffb45273032
jeisten <yeiserna14@gmail.com>
2026-09-04 09:46:18 -0500

    version de responder la ambulacia

--- app/Http/Controllers/Api/DriverController.php
+++ app/Http/Controllers/Api/DriverController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Ambulance;
 use App\Models\Remission;
 use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
